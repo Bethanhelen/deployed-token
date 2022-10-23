6a51061c8a47fd2a866089bd3d7734cd7f2fb42f
bethanhelen8 <79664947+Bethanhelen@users.noreply.github.com>
2022-10-23 19:59:33 +0100

    updated codes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
-
-const ALCHEMY_API_KEY = "6piHhA9VKwxp7GOuHJ_9B4QargwXpE5m";
-const GOERLI_PRIVATE_KEY = "b1c016d8640e61d716378fea9fa98f86126efcbf26fca9064f252ea34697fe5b";
+require("dotenv").config();
 
 module.exports = {
     solidity: "0.8.0",
     networks: {
         goerli: {
-            url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-            accounts: [GOERLI_PRIVATE_KEY]
+            url: process.env.INFURA_GOERLI_ENDPOINT,
+            accounts: [process.env.PRIVATE_KEY]
         }
     }
 };

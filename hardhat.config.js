c7314c0f6d3b0d85129c59ee9d0aaa0c8c6e4fac
bethanhelen8 <79664947+Bethanhelen@users.noreply.github.com>
2022-10-23 20:28:21 +0100

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+import { HardhatUserConfig } from "hardhat/config";
+
+
 module.exports = {
     solidity: "0.8.0",
     networks: {
